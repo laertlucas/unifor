50d5caac6dfdc262ba1300979fa59b4a361179c8
laertlucas <lucaslaert@hotmail.com>
2023-09-06 11:51:00 -0300

    Back-end inicializado e testado com banco H2

---
+++ crud-spring/src/CorsConfig.java
@@ -0,0 +1,23 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // Permitir solicitações de todas as origens (isso pode ser ajustado para limitar a origem)
+
+        // Configurações adicionais, como métodos HTTP permitidos, cabeçalhos etc.
+        // config.addAllowedMethod("GET");
+        // config.addAllowedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
